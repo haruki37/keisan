b7e36d5b6c2de6422e2353081194a19692fc1fea
Haruki Mifuji <harukimifuji@HarukinoMacBook-Air.local>
2020-06-30 23:29:10 +0900

    add new file

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # keisan
-# keisan
--- tenkai.py
+++ tenkai.py
@@ -13,68 +13,6 @@ ncm = 1
 ndp = 10
 ndm = -10
 
-with open("kadai/ensyu-tenkai.tex","w") as f:
-    print(r"\documentclass[a4j,10pt,fleqn]{jarticle}", file = f)
-    print(r"\usepackage[dvipdfmx]{graphicx}", file = f)
-    print(r"% \usepackage[dvipdfmx]{color}", file = f)
-    print(r"\usepackage{here}", file = f)
-    print(r"\usepackage{longtable}", file = f)
-    print(r"\usepackage{bm}", file = f)
-    print(r"\usepackage{amsmath,amssymb}", file = f)
-    print(r"\usepackage{float}", file = f)
-    print(r"\usepackage{txfonts}", file = f)
-    print(r"\usepackage{mathrsfs}", file = f)
-    print(r"\usepackage{subcaption}", file = f)
-    print(r"\usepackage{lscape}", file = f)
-    print(r"\usepackage{textcomp}", file = f)
-    print(r"\usepackage{multirow}", file = f)
-    print(r"\usepackage{afterpage}", file = f)
-    print(r"\usepackage{color}", file = f)
-    print(r"\usepackage{tabularx}", file = f)
-    print(r"\usepackage{pythontex}", file = f)
-#
-#    print(r"\title{ensyu}", file = f)
-#    print(r"\author{Haruki Mifuji}", file = f)
-#    print(r"\date{\today}", file = f)
-#
-    print(r"\begin{document}", file = f)
-#    print(r"\maketitle", file = f)
-#
-#    print(r"\section{ensyu}", file = f)
-#
-    print("tip: $(ax + b)(cx + d) = acx^2 + (ad + bc)x + bd$\n", file = f)
-#
-with open("kadai/ans-tenkai.tex","w") as g:
-    print(r"\documentclass[a4j,10pt,fleqn]{jarticle}", file = g)
-    print(r"\usepackage[dvipdfmx]{graphicx}", file = g)
-    print(r"% \usepackage[dvipdfmx]{color}", file = g)
-    print(r"\usepackage{here}", file = g)
-    print(r"\usepackage{longtable}", file = g)
-    print(r"\usepackage{bm}", file = g)
-    print(r"\usepackage{amsmath,amssymb}", file = g)
-    print(r"\usepackage{float}", file = g)
-    print(r"\usepackage{txfonts}", file = g)
-    print(r"\usepackage{mathrsfs}", file = g)
-    print(r"\usepackage{subcaption}", file = g)
-    print(r"\usepackage{lscape}", file = g)
-    print(r"\usepackage{textcomp}", file = g)
-    print(r"\usepackage{multirow}", file = g)
-    print(r"\usepackage{afterpage}", file = g)
-    print(r"\usepackage{color}", file = g)
-    print(r"\usepackage{tabularx}", file = g)
-    print(r"\usepackage{pythontex}", file = g)
-#
-#    print(r"\title{ensyu}", file = g)
-#    print(r"\author{Haruki Mifuji}", file = g)
-#    print(r"\date{\today}", file = g)
-#
-    print(r"\begin{document}", file = g)
-#    print(r"\maketitle", file = g)
-#
-#    print(r"\section{ensyu}", file = g)
-#
-    print("tip: $(ax + b)(cx + d) = acx^2 + (ad + bc)x + bd$\n", file = g)
-
 i = 1 
 while i <= n:
     a = random.randrange(nam,nap)
@@ -288,16 +226,5 @@ while i <= n:
 #            print(B)
 #            print(C)
         print("$"+ans+"$\n\n") 
-
-        with open("kadai/ensyu-tenkai.tex","a") as f:
-            print("("+str(i)+")~~$"+ensyu+"$\n", file = f)
-        with open("kadai/ans-tenkai.tex","a") as g:
-            print("("+str(i)+")~~$"+ensyu+"$", file = g)
-            print("$$"+ans+"$$\n", file = g) 
-
+#
     i = i + 1
-
-with open("kadai/ensyu-tenkai.tex","a") as f:
-    print(r"\end{document}", file = f)
-with open("kadai/ans-tenkai.tex","a") as g:
-    print(r"\end{document}", file = g)
